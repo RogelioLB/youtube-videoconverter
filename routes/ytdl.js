cb0fbd7959a43b52656ba27ebb854186603fb66a
Rogelio <rogelio20052011@gmail.com>
2020-11-22 17:33:17 -0600

    two

--- routes/ytdl.js
+++ routes/ytdl.js
@@ -9,7 +9,7 @@ const routes=Router();
 routes.post("/",(req,res)=>{
   const {uri}=req.body;
     let r=ytdl(uri)
-    .pipe(fs.createWriteStream(path.resolve(__dirname,`public/video.mp3`)))
+    .pipe(fs.createWriteStream(path.resolve(__dirname,`../public/video.mp3`)))
     if(r) res.send({op:true});
     else res.send({op:false});
   });
