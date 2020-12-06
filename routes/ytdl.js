7d969913bfebcadcc110bec9b87d020b9ca3e7b6
Rogelio <rogelio20052011@gmail.com>
2020-12-06 14:43:46 -0600

    all

--- routes/ytdl.js
+++ routes/ytdl.js
@@ -10,6 +10,10 @@ const {Router}=require('express');
 
 const routes=Router();
 
+function removeEmojis (string) {
+  var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+  return string.replace(regex, '');
+}
 routes.post("/", async(req,res)=>{
   const {uri,op,id}=req.body;
   var io=req.app.get("socket");
@@ -24,7 +28,9 @@ routes.post("/", async(req,res)=>{
     merged: { frame: 0, speed: '0x', fps: 0 },
   };
   var songinfo=await ytdl.getInfo(ref);
-  let nombre=songinfo.videoDetails.title;
+    let nombre = songinfo.videoDetails.title;
+    nombre=removeEmojis(nombre);
+    console.log(nombre);
 if(op=='Video'){
   
   // Get audio and video stream going
@@ -103,9 +109,8 @@ if(op=='Video'){
   audio.pipe(ffmpegProcess.stdio[4]);
   video.pipe(ffmpegProcess.stdio[5]);
   ffmpegProcess.stdio[6].pipe(fs.createWriteStream(path.resolve(__dirname,`../public/${nombre}.mkv`)));
-}if(op=='Audio'){
-
-  ytdl(ref, { filter: 'audioonly', quality: 'highestaudio' })
+    } if (op == 'Audio') {
+  ytdl(ref, { filter: 'audioonly' ,quality:'highestaudio'})
     .on('progress', (_, downloaded, total) => {
       tracker.audio = { downloaded, total };
       io.to(id).emit("upload",{downloaded:(tracker.audio.downloaded/tracker.audio.total*100).toFixed(2)})
@@ -114,11 +119,9 @@ if(op=='Video'){
     }).pipe(fs.createWriteStream(path.resolve(__dirname,`../public/${nombre}.mp3`)));
   
 
-var iD=ytdl.getVideoID(ref);
-
-
+    var iD=ytdl.getVideoID(ref);
 }
-res.send({op:true,id:iD,names:nombre,title:songinfo.videoDetails.title});
+res.send({op:true,id:iD,names:nombre,title:nombre});
 }
 });
 module.exports=routes;
